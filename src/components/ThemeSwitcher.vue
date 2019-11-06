392d14b9d4082a83929702ffec7800b036385248
Daniel Kalivoda <danek.kalivoda@gmail.com>
2019-11-06 21:58:06 +0100

    try to fix theme switcher on mobile

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -1,9 +1,7 @@
 <template>
-<div>
-  <a href="#" class="text-gray-500 dark:text-gray-600 hover:text-green-500 fixed left-0 bottom-0 z-50 border border-transparent rounded-full w-12 h-12 ml-2 sm:ml-10 mb-3 sm:mb-8 z-20 bg-gray-200 dark:bg-gray-900 flex justify-center items-center transition-all" @click.prevent="toggleTheme">
-    <svg v-if="theme === 'mode-dark'" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42"/></svg>
-    <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/></svg>
-  </a>
+<div class="text-gray-500 dark:text-gray-600 hover:text-green-500 fixed left-0 bottom-0 z-50 border border-transparent rounded-full w-12 h-12 ml-2 sm:ml-10 mb-3 sm:mb-8 z-20 bg-gray-200 dark:bg-gray-900 flex justify-center items-center cursor-pointer transition-all" @click.prevent="toggleTheme">
+  <svg v-if="theme === 'mode-dark'" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42"/></svg>
+  <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/></svg>
 </div>
 </template>
 
